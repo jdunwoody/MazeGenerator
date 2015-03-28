1be8b3cdbf0b3ca7e1b1c9b1f3f15b557d2c099d
James Dunwoody <jdunwoody@odecee.com.au>
2015-03-28 12:43:52 +1100

    Initial commit

---
+++ MazeGenerator/Maze/Maze.swift
@@ -0,0 +1,13 @@
+//
+//  Maze.swift
+//  MazeGenerator
+//
+//  Created by James Dunwoody on 28/03/2015.
+//  Copyright (c) 2015 James Dunwoody. All rights reserved.
+//
+
+import UIKit
+
+class Maze: NSObject {
+   
+}
---
+++ MazeGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MazeGenerator
+//
+//  Created by James Dunwoody on 28/03/2015.
+//  Copyright (c) 2015 James Dunwoody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
